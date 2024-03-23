b641617f02626ffd6e58cca5b9c9e2a10855b550
t0nn1x <antoonhrobust@gmail.com>
2024-03-23 16:18:09 +0100

    Create Layout for guest users

--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -1,91 +1,72 @@
 <template>
-    <div
-        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-    >
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-                class="mx-auto h-10 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-            />
-            <h2
-                class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-            >
-                Set new password
-            </h2>
-        </div>
-
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" method="POST">
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label
-                            for="new-password"
-                            class="block text-sm font-medium leading-6 text-gray-900"
-                            >New Password</label
-                        >
-                    </div>
-                    <div class="mt-2">
-                        <input
-                            id="new-password"
-                            name="new_password"
-                            type="password"
-                            required=""
-                            placeholder="New Password"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label
-                            for="password-repeat"
-                            class="block text-sm font-medium leading-6 text-gray-900"
-                            >Confirm new password</label
-                        >
-                    </div>
-                    <div class="mt-2">
-                        <input
-                            id="password-repeat"
-                            name="password_repeat"
-                            type="password"
-                            required=""
-                            placeholder="Confirm new password"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div class="flex items-center justify-between">
-                        <div class="text-sm">
-                            <router-link
-                                to="/login"
-                                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                                >Go back to login</router-link
-                            >
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <button
-                        type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <GuestLayout title="Reset password">
+        <form class="space-y-6" action="#" method="POST">
+            <div>
+                <div class="flex items-center justify-between">
+                    <label
+                        for="new-password"
+                        class="block text-sm font-medium leading-6 text-gray-900"
+                        >New Password</label
                     >
-                        Submit
-                    </button>
                 </div>
-            </form>
-        </div>
-    </div>
+                <div class="mt-2">
+                    <input
+                        id="new-password"
+                        name="new_password"
+                        type="password"
+                        required=""
+                        placeholder="New Password"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <label
+                        for="password-repeat"
+                        class="block text-sm font-medium leading-6 text-gray-900"
+                        >Confirm new password</label
+                    >
+                </div>
+                <div class="mt-2">
+                    <input
+                        id="password-repeat"
+                        name="password_repeat"
+                        type="password"
+                        required=""
+                        placeholder="Confirm new password"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <div class="text-sm">
+                        <router-link
+                            to="/login"
+                            class="font-semibold text-indigo-600 hover:text-indigo-500"
+                            >Go back to login</router-link
+                        >
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <button
+                    type="submit"
+                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
+    </GuestLayout>
 </template>
 
-<script>
-export default {
-    name: "Login",
-};
+<script setup>
+import GuestLayout from '../components/GuestLayout.vue'
 </script>
 
 <style scoped></style>
