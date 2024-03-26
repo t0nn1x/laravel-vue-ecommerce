09b3baede3daaffd13f91c917e396790fc76bdc1
t0nn1x <antoonhrobust@gmail.com>
2024-03-26 15:40:17 +0100

    FIx Admin Auth

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum', 'admin')
+Route::middleware('auth:sanctum')
     ->group(function () {
-        Route::get('/user', function (Request $request) {
-            return $request->user();
+        Route::middleware('admin')->group(function () {
+            Route::get('/user', function (Request $request) {
+                return $request->user();
+            });
+            Route::post('/logout', [AuthController::class, 'logout']);
         });
-        Route::post('/logout', [AuthController::class, 'logout']);
     });
 
-Route::post('/login', ['App\Http\Controllers\AuthController', 'login']);
+Route::post('/login', [AuthController::class, 'login']);
