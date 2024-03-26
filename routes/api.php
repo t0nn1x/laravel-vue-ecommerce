d86133639785898461e45d4c19177023651abbef
t0nn1x <antoonhrobust@gmail.com>
2024-03-27 00:43:25 +0100

    Add Splash Screen Loader and Output User in Navbar

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')
     ->group(function () {
         Route::middleware(Admin::class)->group(function () {
-            Route::get('/user', function (Request $request) {
-                return $request->user();
-            });
+            Route::get('/user', [AuthController::class, 'getUser']);
             Route::post('/logout', [AuthController::class, 'logout']);
         });
     });
