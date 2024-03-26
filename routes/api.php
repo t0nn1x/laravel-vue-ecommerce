332401333d55427991974ff210be68d3cf39e397
t0nn1x <antoonhrobust@gmail.com>
2024-03-27 00:01:00 +0100

    Fix Bug with Admin middleware

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')
     ->group(function () {
-        Route::middleware('admin')->group(function () {
+        Route::middleware(Admin::class)->group(function () {
             Route::get('/user', function (Request $request) {
                 return $request->user();
             });
