a105ab1c2b0728e4b47f6f53db7abb7d619d1660
t0nn1x <antoonhrobust@gmail.com>
2024-03-27 18:29:21 +0100

    Create Vue.js Spinner Component

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -19,26 +19,12 @@
         </div>
     </div>
     <div v-else>
-        <div class="flex flex-col items-center">
-            <div class="flex items-center justify-center h-screen">
-                <div
-                    class="inline-block h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-black dark:text-gray-400 mr-2"
-                    role="status"
-                >
-                    <span
-                        class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                        >Loading...</span
-                    >
-                </div>
-                <div class="ml-2">
-                    Please Wait...
-                </div>
-            </div>
-        </div>
+        <Spinner />
     </div>
 </template>
 
 <script setup>
+import Spinner from './core/Spinner.vue'
 import { ref, onMounted, onUnmounted, computed } from "vue";
 import Navbar from "./Navbar.vue";
 import Sidebar from "./Sidebar.vue";
---
+++ backend/src/components/core/Spinner.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex flex-col items-center">
+        <div class="flex items-center justify-center h-screen">
+            <div
+                class="inline-block h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-black dark:text-gray-400 mr-2"
+                role="status"
+            >
+                <span
+                    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                    >Loading...</span
+                >
+            </div>
+            <div class="ml-2">Please Wait...</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Spinner"
+}
+</script>
+
+<style scoped></style>
